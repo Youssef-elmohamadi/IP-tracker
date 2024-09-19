69aeab5690a21bd0e3a856b231e83cafcdf3872f
Youssef-elmohamadi <youssefelmohammadi24@gmail.com>
2024-09-19 23:15:17 +0300

    https api

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ function updateMap(lat, lon) {
 
 function searchIp() {
   let ip = document.querySelector(".search input").value;
-  fetch(`http://ip-api.com/json/${ip}`)
+  fetch(`https://ip-api.com/json/${ip}`)
     .then((response) => response.json())
     .then((data) => {
       if (data.status === "fail") {
